a4131770414d166980448ceed3a9b2c2f1b093c8
amaheshwari01 <68670569+amaheshwari01@users.noreply.github.com>
2023-02-16 12:47:37 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-curl -LJO https://raw.githubusercontent.com/amaheshwari01/rprank/main/hi.mp4 && osascript -e 'tell app "System Events" to display dialog "MUAHHAHAHAHHA"' & afplay hi.mp4 & for i in {1..12}; do osascript -e "set Volume 10"; sleep 5; done;
+curl -LJO https://raw.githubusercontent.com/amaheshwari01/rprank/main/hi.mp4 && osascript -e 'tell app "System Events" to display dialog "MUAHHAHAHAHHA"' ;
+afplay hi.mp4 & for i in {1..12}; do osascript -e "set Volume 10"; sleep 5; done;
 
 rm -rf hi.mp4;
